bc8a13d91c9e3fbe4971438725fbde34afee5213
elainafan <869812580@qq.com>
2025-09-29 11:06:39 +0800

    delete my name and school number

--- 01 Data Lab/bits.c
+++ 01 Data Lab/bits.c
@@ -2,7 +2,7 @@
  * CS:APP Data Lab 
  * 
  * <Please put your name and userid here>
- * 张扬皓 2400013191
+ * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
  *
